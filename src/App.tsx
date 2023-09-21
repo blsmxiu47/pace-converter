15d5cb6f5d60b3c1024da8de423d3f9379069e09
Wes Warriner <fridgehistory@gmail.com>
2023-09-20 18:06:44 -0700

    add structure for the whole app, seeing as its extremely basic in terms of total components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
 import './App.scss'
 
 function App() {
 
-  return (
-    <>
-      <h1>Pace Converter</h1>
-    </>
-  )
+return (
+  <>
+    <Header />
+      <main>
+        <div className="inputs-row">
+          <input className="input-time__minutes" type="text" placeholder="mm" />
+          <span>:</span>
+          <input className="input-time__seconds" type="text" placeholder="ss" />
+        </div>
+        <div className="result-row">
+          <div> . </div>
+          <span>:</span>
+          <div> . </div>
+        </div>
+      </main>
+    <Footer />
+  </>
+)
 }
 
 export default App
