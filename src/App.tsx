2f88dff5578eb83044d66f49332fb25ba5eae522
Wes Warriner <fridgehistory@gmail.com>
2023-09-21 03:37:48 -0700

    move styles to App.scss since we really only need one file

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,24 @@ import './App.scss'
 
 function App() {
 
-return (
-  <>
-    <Header />
-      <main>
-        <div className="inputs-row">
-          <input className="input-time__minutes" type="text" placeholder="mm" />
-          <span>:</span>
-          <input className="input-time__seconds" type="text" placeholder="ss" />
-        </div>
-        <div className="result-row">
-          <div> . </div>
-          <span>:</span>
-          <div> . </div>
-        </div>
-      </main>
-    <Footer />
-  </>
-)
+  return (
+    <>
+      <Header />
+        <main>
+          <div className="inputs-row">
+            <input className="input-time" type="text" placeholder="mm" />
+            <span>:</span>
+            <input className="input-time" type="text" placeholder="ss" />
+          </div>
+          <div className="result-row">
+            <div> . </div>
+            <span>:</span>
+            <div> . </div>
+          </div>
+        </main>
+      <Footer />
+    </>
+  )
 }
 
 export default App
