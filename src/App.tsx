203654bcea51e186b0f5bee35cf341188b43c550
Wes Warriner <fridgehistory@gmail.com>
2023-09-22 18:07:07 -0700

    make converter inputs numeric; provide wrapper section for the converter

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,18 @@ function App() {
     <>
       <Header />
         <main>
-          <div className="inputs-row">
-            <input className="input-time" type="text" placeholder="mm" />
-            <span>:</span>
-            <input className="input-time" type="text" placeholder="ss" />
-          </div>
-          <div className="result-row">
-            <div> . </div>
-            <span>:</span>
-            <div> . </div>
-          </div>
+          <section className="converter">
+            <div className="inputs-row">
+              <input className="input-time" type="number" min="0" max="99" placeholder="mm" />
+              <span>:</span>
+              <input className="input-time" type="number" min="0" max="59" placeholder="ss" />
+            </div>
+            <div className="inputs-row">
+              <input className="input-time" type="number" min="0" max="99" placeholder="mm" />
+                <span>:</span>
+              <input className="input-time" type="number" min="0" max="59" placeholder="ss" />
+            </div>
+          </section>
         </main>
       <Footer />
     </>
