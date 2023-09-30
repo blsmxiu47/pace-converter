c648d60596548024ee83435d7040599c3f011673
Wes Warriner <fridgehistory@gmail.com>
2023-09-29 23:29:07 -0700

    add Quantico font as default for app html; slightly style app header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import PaceConverter from './components/PaceConverter'
 import Footer from './components/Footer'
+import './fonts/Quantico-Regular.ttf'
 import './App.scss'
 
 function App() {
