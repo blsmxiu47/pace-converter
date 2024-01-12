9029d4f0a419b2e8c02483f2136c6d71fe687c86
Wes Warriner <fridgehistory@gmail.com>
2024-01-12 08:59:33 -0700

    init navbar and pacetimeconverter

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+export default function Navbar() {
+    return (
+        <nav className="navtabs">
+            <ul className="navlist">
+                <li>
+                    <button className="nav-button">
+                        Pace Converter
+                    </button>
+                </li>
+                <li>
+                    <button className="nav-button">
+                        Pace Time Converter
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    )
+}
---
+++ src/components/PaceTimeConverter.tsx
@@ -0,0 +1,9 @@
+/* Component for converting min/km or min/mi pace to race times (e.g. 5k, 10k, half marathon, marathon) */
+
+export default function PaceTimeConverter() {
+    return (
+        <div>
+            Do a thing
+        </div>
+    )
+}
