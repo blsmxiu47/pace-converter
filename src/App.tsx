3d1e817131a69a3b0af615ce7d9a56320ce66e43
Wes Warriner <fridgehistory@gmail.com>
2024-01-12 10:04:46 -0700

    refactor App to use Content component

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,14 @@ import './fonts/Quantico-Regular.ttf'
 import './App.scss'
 
 import Header from './components/Header'
-import Navbar from './components/Navbar'
-import PaceConverter from './components/PaceConverter'
-import PaceTimeConverter from './components/PaceTimeConverter'
+import Content from './components/Content'
 import Footer from './components/Footer'
 
 function App() {
-
   return (
     <>
       <Header />
-      <main>
-        <Navbar />
-        <PaceConverter />
-        <PaceTimeConverter />
-      </main>
+      <Content />
       <Footer />
     </>
   )
