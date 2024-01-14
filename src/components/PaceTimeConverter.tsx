6b098f99bd52f148cd4c0631bade1c9306a916c5
Wes Warriner <fridgehistory@gmail.com>
2024-01-14 14:53:53 -0700

    add handling for race time hours fields

--- src/components/PaceTimeConverter.tsx
+++ src/components/PaceTimeConverter.tsx
@@ -12,12 +12,16 @@ const debounceDelay = 200;
 export default function PaceTimeConverter() {
     const [minPerKmMin, setMinPerKmMin] = useState<number>(0);
     const [minPerKmSec, setMinPerKmSec] = useState<number>(0);
+    const [fiveKTimeHr, setFiveKTimeHr] = useState<number>(0);
     const [fiveKTimeMin, setFiveKTimeMin] = useState<number>(0);
     const [fiveKTimeSec, setFiveKTimeSec] = useState<number>(0);
+    const [tenKTimeHr, setTenKTimeHr] = useState<number>(0);
     const [tenKTimeMin, setTenKTimeMin] = useState<number>(0);
     const [tenKTimeSec, setTenKTimeSec] = useState<number>(0);
+    const [halfMarathonTimeHr, setHalfMarathonTimeHr] = useState<number>(0);
     const [halfMarathonTimeMin, setHalfMarathonTimeMin] = useState<number>(0);
     const [halfMarathonTimeSec, setHalfMarathonTimeSec] = useState<number>(0);
+    const [marathonTimeHr, setMarathonTimeHr] = useState<number>(0);
     const [marathonTimeMin, setMarathonTimeMin] = useState<number>(0);
     const [marathonTimeSec, setMarathonTimeSec] = useState<number>(0);
 
@@ -36,16 +40,20 @@ export default function PaceTimeConverter() {
         if (source === 'km' && debouncedMinPerKmMin !== null && debouncedMinPerKmSec !== null && isKmSecValid) {
             const totalSecondsPerKm = debouncedMinPerKmMin * 60 + debouncedMinPerKmSec;
 
-            setFiveKTimeMin(Math.floor(totalSecondsPerKm * 5 / 60));
+            setFiveKTimeHr(Math.floor(totalSecondsPerKm * 5 / 3600));
+            setFiveKTimeMin(Math.floor(totalSecondsPerKm * 5 % 3600 / 60));
             setFiveKTimeSec(Math.round(totalSecondsPerKm * 5 % 60));
 
-            setTenKTimeMin(Math.floor(totalSecondsPerKm * 10 / 60));
+            setTenKTimeHr(Math.floor(totalSecondsPerKm * 10 / 3600));
+            setTenKTimeMin(Math.floor(totalSecondsPerKm * 10 % 3600 / 60));
             setTenKTimeSec(Math.round(totalSecondsPerKm * 10 % 60));
 
-            setHalfMarathonTimeMin(Math.floor(totalSecondsPerKm * KM_TO_HM / 60));
+            setHalfMarathonTimeHr(Math.floor(totalSecondsPerKm * KM_TO_HM / 3600));
+            setHalfMarathonTimeMin(Math.floor(totalSecondsPerKm * KM_TO_HM % 3600 / 60));
             setHalfMarathonTimeSec(Math.round(totalSecondsPerKm * KM_TO_HM % 60));
 
-            setMarathonTimeMin(Math.floor(totalSecondsPerKm * KM_TO_M / 60));
+            setMarathonTimeHr(Math.floor(totalSecondsPerKm * KM_TO_M / 3600));
+            setMarathonTimeMin(Math.floor(totalSecondsPerKm * KM_TO_M % 3600 / 60));
             setMarathonTimeSec(Math.round(totalSecondsPerKm * KM_TO_M % 60));
         }
     }, [source, debouncedMinPerKmMin, debouncedMinPerKmSec, isKmSecValid]);
@@ -82,11 +90,54 @@ export default function PaceTimeConverter() {
 
     // array of timeMin and timeSec values to display
     const timeValues = [
-        { label: '5k', timeMin: fiveKTimeMin, timeSec: fiveKTimeSec },
-        { label: '10k', timeMin: tenKTimeMin, timeSec: tenKTimeSec },
-        { label: 'half marathon', timeMin: halfMarathonTimeMin, timeSec: halfMarathonTimeSec },
-        { label: 'marathon', timeMin: marathonTimeMin, timeSec: marathonTimeSec },
+        { label: '5k', timeHr: fiveKTimeHr, fiveKTimeHr, timeMin: fiveKTimeMin, timeSec: fiveKTimeSec },
+        { label: '10k', timeHr: tenKTimeHr, timeMin: tenKTimeMin, timeSec: tenKTimeSec },
+        { label: 'half marathon', timeHr: halfMarathonTimeHr, timeMin: halfMarathonTimeMin, timeSec: halfMarathonTimeSec },
+        { label: 'marathon', timeHr: marathonTimeHr, timeMin: marathonTimeMin, timeSec: marathonTimeSec },
     ];
+
+    function hourMinuteInputs(timeHr: number, timeMin: number) {
+        if (timeHr > 0) {
+            return (
+                <>
+                    <input
+                        type="number"
+                        pattern="\d{0,2}"
+                        className='input-value'
+                        value={unpadZero(timeHr)}
+                        onFocus={handleFocus} 
+                        // onChange={handleTimeSecChange}
+                        min={0}
+                        max={99}
+                    />
+                    <span>:</span>
+                    <input
+                        type="number"
+                        pattern="\d{0,2}"
+                        className='input-value'
+                        value={padZero(timeMin)}
+                        onFocus={handleFocus} 
+                        // onChange={handleTimeSecChange}
+                        min={0}
+                        max={59}
+                    />
+                </>
+            )
+        } else {
+            return (
+                <input
+                    type="number"
+                    pattern="\d{0,2}"
+                    className='input-value'
+                    value={unpadZero(timeMin)}
+                    onFocus={handleFocus} 
+                    // onChange={handleTimeSecChange}
+                    min={0}
+                    max={99}
+                />
+            )
+        }
+    }
 
     return (
         <div className="converter">
@@ -120,20 +171,11 @@ export default function PaceTimeConverter() {
             </div>
             <div className="inputs-grid">
                 {timeValues.map((time, i) => (
-                    <div className='input-group' key={i}>
+                    <div className='input-group inputs-grid-item' key={i}>
                         <label>
                             {time.label}
                             <div className='time-input'>
-                                <input
-                                    type="number"
-                                    pattern="\d{0,2}"
-                                    className='input-value'
-                                    value={unpadZero(time.timeMin)}
-                                    onFocus={handleFocus} 
-                                    // onChange={handleTimeSecChange}
-                                    min={0}
-                                    max={99}
-                                />
+                                {hourMinuteInputs(time.timeHr, time.timeMin)}
                                 <span>:</span>
                                 <input
                                     type="number"
@@ -149,32 +191,6 @@ export default function PaceTimeConverter() {
                         </label>
                     </div>
                 ))}
-                    {/* <label>
-                        5k Time
-                        <div className='time-input'>
-                            <input
-                                type="number"
-                                pattern="\d{0,2}"
-                                className='input-value'
-                                value={unpadZero(fiveKTimeMin)}
-                                onFocus={handleFocus} 
-                                // onChange={handleTimeSecChange}
-                                min={0}
-                                max={99}
-                            />
-                            <span>:</span>
-                            <input
-                                type="number"
-                                pattern="\d{0,2}"
-                                className='input-value'
-                                value={padZero(fiveKTimeSec)}
-                                onFocus={handleFocus} 
-                                // onChange={handleTimeSecChange}
-                                min={0}
-                                max={59}
-                            />
-                        </div>
-                    </label> */}
             </div>
         </div>
     )
