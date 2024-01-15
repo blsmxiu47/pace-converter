ab973919187c62206f35949ca80223564459359a
Wes Warriner <fridgehistory@gmail.com>
2024-01-14 18:22:46 -0700

    add util functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,14 @@
-export const padZero = (num: number) => (num < 10 ? `0${num}` : `${num}`);
+export const padZero = (num: number) => {
+    num = Math.round(num);
+    if (num < 10) {
+        return `0${num}`;
+    } else {
+        return `${num}`;
+    }
+}
 
 export const unpadZero = (num: number) => {
+    num = Math.round(num);
     if (num === 0) {
         return '0';
     }
@@ -9,4 +17,22 @@ export const unpadZero = (num: number) => {
 
 export const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
     e.target.select();
+}
+
+const timeTypes = ['minute', 'second'];
+
+export const validateTimeInput = (value: string, type: typeof timeTypes[number]) => {
+    const valueStr = value.toString().replace(/^0+/, '');
+    if (!/^\d{0,2}$/.test(valueStr)) {
+        return;
+    }
+    const num = valueStr === '' ? 0 : parseInt(valueStr, 10);
+    let max_num = 99;
+    if (type === 'second') {
+        max_num = 59;
+    }
+    if (isNaN(num) || num < 0 || num > max_num) {
+        return;
+    }
+    return num;
 }
